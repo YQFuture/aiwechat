5313823798ba11f8db55ff39e50e5f1ae3ae5b7c
“zouxiaodong” <13575232531@163.com>
2024-06-19 16:24:28 +0800

    添加客户端断开连接自动登出

--- handle/conn_handler.go
+++ handle/conn_handler.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"github.com/eatmoreapple/openwechat"
 	"github.com/gorilla/websocket"
+	"io"
 	"log"
 )
 
 func ConnHandler(ws *websocket.Conn, bot *openwechat.Bot) {
+	autoLogout(ws, bot)
 	for {
 		// 读取消息
 		_, message, err := ws.ReadMessage()
@@ -24,4 +26,18 @@ func ConnHandler(ws *websocket.Conn, bot *openwechat.Bot) {
 		//分发处理
 		go MessageDispatcher(ws, bot, &messageModel)
 	}
+}
+
+// 自动登出
+func autoLogout(ws *websocket.Conn, bot *openwechat.Bot) {
+	ws.SetCloseHandler(func(code int, text string) error {
+		log.Println("客户端断开ws连接")
+		//bot登出
+		err := bot.Logout()
+		if err != nil && err != io.EOF {
+			log.Println("bot登出失败", err)
+			return err
+		}
+		return nil
+	})
 }
