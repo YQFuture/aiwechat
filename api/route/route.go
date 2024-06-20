cd0dad4f7c70b50cba34f61d5e1896475b59d069
“zouxiaodong” <13575232531@163.com>
2024-06-20 17:13:17 +0800

    添加前端页面代理路由

--- api/route/route.go
+++ api/route/route.go
@@ -3,11 +3,15 @@ package route
 import (
 	api2 "aiwechat/api"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Route(engine *gin.Engine) {
 	aiwechat := engine.Group("/aiwechat")
 	{
+		//websocket接口
 		aiwechat.GET("/conn", api2.Conn)
+		//前端页面
+		aiwechat.StaticFS("/page", http.Dir("./dist"))
 	}
 }
