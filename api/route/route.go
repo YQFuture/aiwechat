5dca46e825e1114c7ad3e4e46dbaaf92446a351f
qq1972474341 <37800602+qq1972474341@users.noreply.github.com>
2024-06-26 12:01:43 +0800

    chore: Add static file serving for frontend assets

--- api/route/route.go
+++ api/route/route.go
@@ -2,16 +2,41 @@ package route
 
 import (
 	api2 "aiwechat/api"
-	"github.com/gin-gonic/gin"
+	"embed"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+//go:embed frontend/dist/*
+var staticFiles embed.FS
+
 func Route(engine *gin.Engine) {
+
+	// 将 embed.FS 类型转换为 http.FileSystem 接口
+	fs := http.FS(staticFiles)
+
+	// 提供 assets 目录中的静态文件
+	engine.GET("/assets/*filepath", func(c *gin.Context) {
+		c.FileFromFS("frontend/dist/assets/"+c.Param("filepath"), fs)
+	})
+
+	// 提供 favicon.ico
+	engine.GET("/favicon.ico", func(c *gin.Context) {
+		c.FileFromFS("frontend/dist/favicon.ico", fs)
+	})
+
+	// 静态web服务
+	engine.GET("/frontend/*filepath", func(c *gin.Context) {
+		c.FileFromFS("frontend/dist/"+c.Param("filepath"), fs)
+	})
+
 	aiwechat := engine.Group("/aiwechat")
 	{
 		//websocket接口
 		aiwechat.GET("/conn", api2.Conn)
 		//前端页面
-		aiwechat.StaticFS("/page", http.Dir("./dist"))
+		// aiwechat.StaticFS("/page", http.Dir("./dist"))
+
 	}
 }
